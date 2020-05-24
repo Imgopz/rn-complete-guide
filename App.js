a8590cecab5798339c3b73ed9e78244761cf4da2
Imgopz <rrgkmech@gmail.com>
2020-05-24 21:14:56 +0530

    text input button, and styles

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View></View>
-      <View></View>
+    <View style={{ padding: 30}}>
+      <View>
+        <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1}} />
+      </View>
+      <View>
+        <Button title="ADD" />
+      </View>
     </View>
   );
 }
